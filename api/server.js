64519333a26feef54d49efb6c11eb987fe3e7efc
Javier Umana <javierumanafs@gmail.com>
2020-05-23 19:58:24 -0400

    test

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const helmet = require("helmet");
+const helmet = require("helmet");
 const cors = require("cors");
 
 const authRouter = require("../auth/auth-router.js");
@@ -7,15 +7,16 @@ const recipesRouter = require("../recipes/recipes-router.js");
 
 const server = express();
 
-// server.use(helmet());
-server.use(express.json());
 server.use(cors());
+server.use(helmet());
+server.use(express.json());
+
 
 server.use("/api/auth", authRouter);
 server.use("/api/recipes", recipesRouter);
 
-server.get("/", (req, res) =>{
-  res.json({ api: "up"});
+server.get("/api/tester", (req, res) =>{
+  res.json({ api: "test running"});
 });
 
 module.exports = server;
