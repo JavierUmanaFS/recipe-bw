7fa77f5c7df8cda3eb99a779e640d7adc5c6d8c1
Javier Umana <javierumanafs@gmail.com>
2020-05-23 19:43:31 -0400

    test

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const helmet = require("helmet");
+// const helmet = require("helmet");
 const cors = require("cors");
 
 const authRouter = require("../auth/auth-router.js");
@@ -7,7 +7,7 @@ const recipesRouter = require("../recipes/recipes-router.js");
 
 const server = express();
 
-server.use(helmet());
+// server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
